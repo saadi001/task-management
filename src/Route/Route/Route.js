5dd5f11c5959d1a2a459292dc8e06a46b8abca03
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-28 20:12:25 +0600

    route updated

--- src/Route/Route/Route.js
+++ src/Route/Route/Route.js
@@ -2,7 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import AddTask from "../../pages/AddTask/AddTask";
 import CompletedTask from "../../pages/completedTask/CompletedTask";
+import Login from "../../pages/Login/Login";
 import MyTask from "../../pages/Mytask/MyTask";
+import NotFound from "../../pages/NotFound/NotFound";
+import Signup from "../../pages/Signup/Signup";
 
 export const router = createBrowserRouter([
      {
@@ -26,5 +29,18 @@ export const router = createBrowserRouter([
                     element: <CompletedTask></CompletedTask>
                }
           ]
+     },
+     {
+          path: '/login',
+          element: <Login></Login>
+     },
+     {
+          path: '/signup',
+          element: <Signup></Signup>
+     },
+     {
+          path: '*',
+          element: <NotFound></NotFound>
      }
+
 ])
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Signup = () => {
+     return (
+          <div className='main flex justify-center items-center py-10'>
+               <div className='max-w-4xl w-full h-full mx-3 lg:mx-0 rounded-md inner-card overflow-y-auto'>
+          
+               </div>
+               
+          </div>
+     );
+};
+
+export default Signup;
