e25fe048bd7d0e3ce17064b05800632db77eeec6
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-29 16:34:14 +0600

    task delete operation added

--- src/pages/Mytask/MyTask.js
+++ src/pages/Mytask/MyTask.js
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const MyTask = () => {
      const { user } = useContext(AuthContext)
-     const { data: myTask, isLoading } = useQuery({
+     const { data: myTask, isLoading, refetch } = useQuery({
           queryKey: ['myTask', user?.email],
           queryFn: async () => {
                const res = await fetch(`http://localhost:5000/myTask?email=${user?.email}`)
@@ -14,6 +15,21 @@ const MyTask = () => {
           }
      })
 
+     const handleDelete = (id) =>{
+          fetch(`http://localhost:5000/myTask/${id}`,{
+               method: 'DELETE',
+
+          })
+          .then(res=> res.json())
+          .then(data=>{
+               console.log(data)
+               if(data.deletedCount>0){
+                    refetch()
+                    toast.success('task deleted successfully')
+               }
+          })
+     }
+
      if (isLoading) {
           return <div>Loading...</div>
      }
@@ -22,21 +38,25 @@ const MyTask = () => {
           <div>
                <p className='text-2xl font-semibold capitalize text-gray-700'>My task</p>
                {
-                    myTask && myTask.map(task => <div className='mt-5 flex justify-between items-center border rounded'>
+                    myTask && myTask.map(task => <div key={task._id} className='mt-5 flex justify-between items-center border rounded'>
                          <div className='flex gap-4'>
                               <div className="avatar">
-                                   <div className="w-24 rounded">
-                                        <img src={task?.image} alt='taskPhoto'/>
+                                   <div className="w-24 h-full rounded">
+                                        <img src={task?.image} alt='taskPhoto' />
                                    </div>
                               </div>
                               <div>
                                    <p className='capitalize text-xl font-semibold text-slate-700'>{task?.taskName}</p>
-                                   <p className='text-sm'>{task?.taskDetails.length > 30 ? task?.taskDetails.slice(0,20) + "..." : task?.taskDetails}</p>
+                                   <p className='text-sm'>{task?.taskDetails.length > 30 ? task?.taskDetails.slice(0, 20) + "..." : task?.taskDetails}</p>
                               </div>
                          </div>
-                         <div className='flex gap-2'>
-                              <div>update</div>
-                              <div>delete</div>
+                         <div className='grid grid-cols-2 gap-3 pr-2'>
+                              <button className="px-4 py-2 text-xs text-slate-200 transition-colors duration-300 transform bg-gradient-to-r from-[#cc2b5e] to-[#753a88] rounded-md  focus:outline-none focus:bg-gray-600">update</button>
+                              <button onClick={()=>handleDelete(task._id)} className="px-4 py-2 text-xs text-slate-200 transition-colors duration-300 transform bg-gradient-to-r from-[#cc2b5e] to-[#753a88] rounded-md  focus:outline-none focus:bg-gray-600">delete</button>
+                              <button className="px-4 py-2 text-xs text-slate-200 transition-colors duration-300 transform bg-gradient-to-r from-[#cc2b5e] to-[#753a88] rounded-md  focus:outline-none focus:bg-gray-600">complete</button>
+                              
+
+
                          </div>
                     </div>)
                }
